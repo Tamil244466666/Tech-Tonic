99f6a60850530491f0b5e5531d5dc2e6dde0413b
Tamil Kumaran <tamilkumaran021@gmail.com>
2024-10-21 16:43:20 +0530

    eight commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-import Register_login from './pages/Register-login.js';
+import Register_login from './pages/Register-login.jsx';
 import {Router,Routes,Route} from 'react-router-dom';
 import Navbar from './Components/Navbar.jsx'
 import Login from './Components/Login.jsx';
-import Home from './pages/Home.js';
-import Saved from './pages/Saved.js';
-import Aboutus from './pages/AboutUs.js';
+import Home from './pages/Home.jsx';
+import Saved from './pages/Saved.jsx';
+import Aboutus from './pages/AboutUs.jsx';
 import Footer from './Components/Footer.jsx';
 
 function App() {
